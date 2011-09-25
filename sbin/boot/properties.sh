707270cc0b965737f9147be423624a0e8ea718e8
FrozenData <FrozenData@gmail.com>
2011-09-26 00:34:28 +0530

    First commit by FD

---
+++ sbin/boot/properties.sh
@@ -0,0 +1,13 @@
+/sbin/ext/busybox mount -t rootfs -o remount,rw rootfs 
+mkdir -p /customkernel/property 
+echo true >> /customkernel/property/customkernel.cf-root 
+echo true >> /customkernel/property/customkernel.base.cf-root 
+echo FrozenData >> /customkernel/property/customkernel.name 
+echo "FD 1.0" >> /customkernel/property/customkernel.namedisplay 
+echo 70 >> /customkernel/property/customkernel.version.number 
+echo 1.0 >> /customkernel/property/customkernel.version.name 
+echo true >> /customkernel/property/customkernel.bootani.zip 
+echo true >> /customkernel/property/customkernel.bootani.bin 
+echo true >> /customkernel/property/customkernel.cwm 
+echo 4.0.0.2 >> /customkernel/property/customkernel.cwm.version 
+/sbin/ext/busybox mount -t rootfs -o remount,ro rootfs 
